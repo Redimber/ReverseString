67c7dced8210c029b716b7aeedbec1269fbd315b
Mountasser <Mountasser@ptarget.lan>
2018-03-04 18:29:18 +0000

    Initial Commit

---
+++ ReverseString/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ReverseString
+//
+//  Created by Mountasser on 3/4/18.
+//  Copyright © 2018 Mountasser. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
